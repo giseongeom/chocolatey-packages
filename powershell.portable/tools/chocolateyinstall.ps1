ca8908a1ed57719e2fb8715ea5eab47464ff9456
GiSeong Eom <jurist@kldp.org>
2018-06-14 23:24:47 +0900

    Added powershell.portable 6.0.2.20180614

--- powershell.portable/tools/chocolateyinstall.ps1
+++ powershell.portable/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 # 32-bit
-$url = 'https://github.com/PowerShell/PowerShell/releases/download/v6.1.0-preview.3/PowerShell-6.1.0-preview.3-win-x86.zip'
-$checksum = 'A95C6BA5698ED1530A27A26AB1E24B76F88CBE05FF06C8F493903A23A0B56919'
+$url = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.2/PowerShell-6.0.2-win-x86.zip'
+$checksum = '87048B0A2DBD56AA8FE1F92DDB7D7BBF8E904F8D54EE2A62443C7B31AE9E55F8'
 $checksumType = 'sha256'
 
 # 64-bit
-$url64 = 'https://github.com/PowerShell/PowerShell/releases/download/v6.1.0-preview.3/PowerShell-6.1.0-preview.3-win-x64.zip'
-$checksum64 = '55C849547A03F19E8275BED5221FADB54F97759D6F540243A935D8477EE2C159'
+$url64 = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.2/PowerShell-6.0.2-win-x64.zip'
+$checksum64 = '8CB153E540ED9D9A7FE00CB3D1FE94A0ED089B574FD02E816AB2BB066F4C4F89'
 $checksumType64 = $checksumType
 
 $packageName = 'powershell.portable'

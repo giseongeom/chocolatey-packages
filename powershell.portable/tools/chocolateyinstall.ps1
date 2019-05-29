1f14dc45fbc3aaa1a7fb51fdddc0e32a352e9617
GiSeong Eom <jurist@kldp.org>
2019-05-29 23:21:32 +0900

    powershell.portable 6.2.1

--- powershell.portable/tools/chocolateyinstall.ps1
+++ powershell.portable/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = 'powershell.portable'
     # 32-bit
-    url            = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x86.zip'
-    checksum       = 'DAE9D3D2AD332A82312D2DAFA398287670FB5CBB94128C80D422E1F3679F8EB2'
+    url            = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.1/PowerShell-6.2.1-win-x86.zip'
+    checksum       = 'F8A713A2614603267683F463B75CE9A81756107F0C927F198BBDD747ACB10AC0'
     checksumType   = 'sha256'
     # 64-bit
-    url64bit       = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-6.2.0-win-x64.zip'
-    checksum64     = 'C02AF438D3BC8DC61815110C99189A694ED161E64C64A59B49AB01D945DFC73A'
+    url64bit       = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.1/PowerShell-6.2.1-win-x64.zip'
+    checksum64     = '6BCC0F80CA549A8ADB317B2EC1294F103C4BF75CC29EFA8AC03A27F9A860B1F4'
     checksumType64 = $checksumType
     # misc
     unziplocation  = "$toolsDir\pwsh"

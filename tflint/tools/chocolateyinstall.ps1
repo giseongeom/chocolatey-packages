1cb11d7b0a2a41a72f0f6d31233caa413fce659c
GiSeong Eom <giseong.eom@bluehole.net>
2019-05-30 10:59:53 +0900

    tflint 0.8.0

--- tflint/tools/chocolateyinstall.ps1
+++ tflint/tools/chocolateyinstall.ps1
@@ -5,13 +5,13 @@ $packageArgs = @{
     packageName    = 'tflint'
 
     # 32-bit
-    url            = 'https://github.com/wata727/tflint/releases/download/v0.7.6/tflint_windows_386.zip'
-    checksum       = '86772916467eb294e825c24593ba989639acbb326cc68a6ec9c4cbfbb0bc1127'
+    url            = 'https://github.com/wata727/tflint/releases/download/v0.8.0/tflint_windows_386.zip'
+    checksum       = '04f2cbd2ab5e571fbe048bf2c11d0dca06a1f85413083d5cba22c0a96581344e'
     checksumType   = 'sha256'
 
     # 64-bit
-    url64bit       = 'https://github.com/wata727/tflint/releases/download/v0.7.6/tflint_windows_amd64.zip'
-    checksum64     = '83d1798f63d85f49c9db03192bff9161a67209a6e37309fc978d05a0a70e15b9'
+    url64bit       = 'https://github.com/wata727/tflint/releases/download/v0.8.0/tflint_windows_amd64.zip'
+    checksum64     = '5e3d387ebbac4540341750f3430690cad90ab6482a15de1d81cc7f6721dc99f2'
     checksumType64 = $checksumType
 
     # misc

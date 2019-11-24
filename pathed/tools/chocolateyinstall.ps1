e9549863ac5567d48b9bb7ed89a38a74131f2132
GiSeong Eom <jurist@kldp.org>
2019-11-24 22:15:13 +0900

    Added pathed 4.2

---
+++ pathed/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName     = 'pathed'
+    # 32-bit
+    FileFullPath    = "$toolsDir\pathed.zip"
+    # 64-bit
+    FileFullPath64  = $FileFullPath
+    # misc
+    Destination     = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs

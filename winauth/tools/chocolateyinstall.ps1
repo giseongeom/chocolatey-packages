e39ec4328002236a7a0cec44d88f957013c0ea60
GiSeong Eom <jurist@kldp.org>
2019-12-10 23:43:55 +0900

    Added WinAuth 3.5.1 (built for personal use)

---
+++ winauth/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName     = 'winauth'
+    # 32-bit
+    FileFullPath    = "$toolsDir\winauth.zip"
+    # 64-bit
+    FileFullPath64  = $FileFullPath
+    # misc
+    Destination     = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs

efb2e647476bd24621d0c2afa33af9d77b197001
GiSeong Eom <jurist@kldp.org>
2020-07-18 23:07:10 +0900

    Add base64 1.0.0

---
+++ base64/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$binFileName = 'base64'
+$fileFullPath = Join-Path $toolsDir "$binFileName.exe"
+
+$packageArgs = @{
+    packageName    = 'base64'
+    FileFullPath   = $fileFullPath
+
+    # 64-bit
+    url64bit       = $url
+    checksum64     = $checksum
+    checksumType64 = $checksumType
+
+    # 32-bit
+    url            = 'http://www.rtner.de/software/base64.exe'
+    checksum       = 'a0aba89ebeb995cedff714829cb08cfafc1f8481292702bfa30fc9c5a7bce88f'
+    checksumType   = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
